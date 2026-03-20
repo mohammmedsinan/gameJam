7397b0fe4b22a034938ae59c1b8f394f57911b27
mohammmedsinan <mohammedbusiness1099@gmail.com>
2026-03-20 22:56:40 +0300

    Add SkillCheck functionality and integrate into GameScene

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -1,44 +1,40 @@
 local CrTv = require("src/ui/CrTv");
 
-local Player = {
-}
+local Player = {}
 Player.__index = Player
 
 function Player.new(bus)
-	return setmetatable({
-		name = "player",
-		bus = bus,
-		width = 70,
-		height = 70,
-		health = 10,
-		maxHealth = 10,
-		damage = 1,
-		attack = 1,
-		defence = 0,
-		xp = 0,
-		level = 1,
-		gold = 0,
-	}, Player)
+    return setmetatable({
+        name = "player",
+        bus = bus,
+        width = 70,
+        height = 70,
+        health = 10,
+        maxHealth = 10,
+        damage = 1,
+        attack = 1,
+        defence = 0,
+        xp = 0,
+        level = 1,
+        gold = 0
+    }, Player)
 end
 
 function Player:load()
 end
 
 function Player:update(dt)
-	if love.keyboard.isDown("space") then
-		self.bus:queue("boss:hit", self.damage)
-	end
 end
 
 function Player:draw()
-	love.graphics.setColor(1, 1, 1, 1)
-	local CrTvScreen = CrTv:getCrTvScreenDetails();
-	love.graphics.rectangle("fill", CrTvScreen.border.left + 100, CrTvScreen.border.bottom - 100, self.width, self
-		.height)
-	-- red color for the border
-	love.graphics.setColor(1, 0, 0, 1)
-	love.graphics.rectangle("line", CrTvScreen.border.left + 100, CrTvScreen.border.bottom - 100, self.width, self
-		.height)
+    love.graphics.setColor(1, 1, 1, 1)
+    local CrTvScreen = CrTv:getCrTvScreenDetails();
+    love.graphics.rectangle("fill", CrTvScreen.border.left + 100, CrTvScreen.border.bottom - 100, self.width,
+        self.height)
+    -- red color for the border
+    love.graphics.setColor(1, 0, 0, 1)
+    love.graphics.rectangle("line", CrTvScreen.border.left + 100, CrTvScreen.border.bottom - 100, self.width,
+        self.height)
 end
 
 return Player;
--- src/scenes/GameScene.lua
+++ src/scenes/GameScene.lua
@@ -1,6 +1,7 @@
 local chatBox = require("src/ui/chatBox");
 local shopper = require("src/ui/shopper");
 local CrTv = require("src/ui/CrTv");
+local SkillCheck = require("src/game/skillcheck");
 Player = require("src/entities/player");
 Boss = require("src/entities/boss");
 Signal = require("src/utils/signal")
@@ -9,47 +10,84 @@ local GameScene = {}
 GameScene.__index = GameScene
 
 function GameScene:load()
-	CrTv:load()
-	chatBox:load()
-	shopper:load()
-	boss   = Boss.new(bus)
-	player = Player.new(bus)
+    CrTv:load()
+    chatBox:load()
+    shopper:load()
+    boss = Boss.new(bus)
+    player = Player.new(bus)
+    if SK then
+        SK:load()
+    end
+
 end
 
 function GameScene:update(dt)
-	CrTv:update(dt)
-	chatBox:update(dt)
-	shopper:update(dt)
-	player:update(dt)
-	boss:update(dt)
-	bus:flush()
+    CrTv:update(dt)
+    chatBox:update(dt)
+    shopper:update(dt)
+    player:update(dt)
+    boss:update(dt)
+    bus:flush()
+    if SK then
+        SK:update(dt)
+    end
 end
 
 function GameScene:draw()
-	love.graphics.setColor(0.2, 0.2, 0.2, 0.2)
-	CrTv:draw()
-	chatBox:draw()
-	shopper:draw()
-	player:draw()
-	boss:draw()
+    love.graphics.setColor(0.2, 0.2, 0.2, 0.2)
+    CrTv:draw()
+    chatBox:draw()
+    shopper:draw()
+    player:draw()
+    boss:draw()
+    if SK then
+        SK:draw()
+    end
+end
+
+function love.resize()
 end
 
 function GameScene:keypressed(key)
-	if key == "tab" then
-		SceneManager:switch("menu", {
-			kind = "fade",
-			duration = 0.3
-		})
-	end
+    if key == "tab" then
+        SceneManager:switch("menu", {
+            kind = "fade",
+            duration = 0.3
+        })
+    end
 
-	if key == "escape" then
-		love.event.quit();
-	end
+    if key == "escape" then
+        love.event.quit();
+    end
 
-	if key == "w" then
-		local isFullScreen, _ = love.window.getFullscreen()
-		love.window.setFullscreen(not isFullScreen)
-	end
+    if key == "w" then
+        local isFullScreen, _ = love.window.getFullscreen()
+        love.window.setFullscreen(not isFullScreen)
+    end
+    if (SK) then
+        SK:keypressed(key)
+    end
+    if key == "space" then
+        if SK then
+            SK = nil
+        else
+            SK = SkillCheck:new({
+                successArcSize = math.rad(55),
+                greatArcSize = math.rad(12),
+                onSuccess = function()
+                    print("success!")
+                end,
+                onGreat = function()
+                    print("GREAT!")
+                end,
+                onMiss = function()
+                    print("miss...")
+                end
+            })
+            SK:Spawn()
+        end
+    end
+
 end
 
 return GameScene
